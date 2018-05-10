1abc3fc238b4d99b3eca5559860162d25d289e5b
dfreire <dario.freire@gmail.com>
2018-05-10 19:11:04 +0100

    improved styling

--- client-web/src/components/SelectField.tsx
+++ client-web/src/components/SelectField.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+const { Icon } = require('react-fa');
 
 interface Props {
     label: string;
@@ -10,19 +11,25 @@ interface Props {
 const SelectField = (props: Props) => (
     <div className={classes.field}>
         <label className={classes.label}>{props.label}</label>
-        <select className={classes.select} value={props.value} onChange={evt => props.onChange(evt.target.value)}>
-            {Object.keys(props.valueMap).map(key => (
-                <option key={key} value={props.valueMap[key]}>{key}</option>
-            ))}
-        </select>
-
+        <div className={classes.selectContainer}>
+            <select className={classes.select} value={props.value} onChange={evt => props.onChange(evt.target.value)}>
+                {Object.keys(props.valueMap).map(key => (
+                    <option key={key} value={props.valueMap[key]}>{key}</option>
+                ))}
+            </select>
+            <div className={classes.dropdownIcon}>
+                <Icon name="caret-down" />
+            </div>
+        </div>
     </div>
 );
 
 const classes = {
-    field: "w-full py-2",
-    label: "block pb-2 text-grey-darkest font-medium",
-    select: "block p-2 w-full border rounded",
+    field: 'w-full py-2',
+    label: 'block pb-2 text-grey-darkest font-medium',
+    selectContainer: 'relative block w-full border rounded bg-grey-lighter hover:bg-grey-lightest',
+    select: 'appearance-none inline-block w-full absolute pin p-2 bg-transparent',
+    dropdownIcon: 'inline-block w-full text-right p-2 bg-transparent',
 }
 
 export default SelectField;
