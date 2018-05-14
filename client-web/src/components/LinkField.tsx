3a53508634548754f4c8508418ce96a4852629df
dfreire <dario.freire@gmail.com>
2018-05-14 15:47:46 +0100

    improving LinkField

--- client-web/src/components/LinkField.tsx
+++ client-web/src/components/LinkField.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import * as validator from 'validator';
+const { Icon } = require('react-fa');
 
 interface Props {
     label: string;
@@ -9,14 +11,27 @@ interface Props {
 const LinkField = (props: Props) => (
     <div className={classes.field}>
         <label className={classes.label}>{props.label}</label>
-        <input className={classes.input} type="text" value={props.value} onChange={evt => props.onChange(evt.target.value)} />
+        <div className={classes.inputContainer}>
+            <input className={classes.input} type="text" value={props.value} onChange={evt => props.onChange(evt.target.value)} />
+            {isLink(props.value) &&
+                <a className={classes.linkIcon} href={props.value}><Icon name="external-link" /></a>
+            }
+        </div>
     </div>
 );
 
 const classes = {
     field: "w-full py-2",
     label: "block pb-2 text-grey-darkest font-medium",
-    input: "block p-2 w-full border rounded",
+    inputContainer: "block w-full border rounded flex",
+    input: 'inline-block w-full bg-transparent flex-1 p-2',
+    linkIcon: 'inline-block text-right bg-grey-lightest border-l p-2 text-black no-underline',
+}
+
+function isLink(s: string) {
+    return validator.isURL(s)
+        || s.startsWith('http://localhost')
+        || s.startsWith('https://localhost');
 }
 
 export default LinkField;
