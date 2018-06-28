d22486ff26a3a252ec28c54ab5493310ce443f39
dfreire <dario.freire@gmail.com>
2018-06-28 14:47:11 +0100

    removed 'website' since it was not being used

--- website/src/js/containers/404.js
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-export default () => (React.createElement("div", null,
-    React.createElement("h1", null, "404 - Oh no's! We couldn't find that page :(")));
--- website/src/js/containers/Home.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { withRouteData } from 'react-static';
-export default withRouteData((props) => (React.createElement("div", null,
-    React.createElement("h1", { style: { textAlign: 'center' } },
-        "Welcome to ",
-        props.title,
-        " ",
-        props.pageTitle))));
