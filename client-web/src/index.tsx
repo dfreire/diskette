055be26d1fabdfcc808c5628815130e816a18105
dfreire <dario.freire@gmail.com>
2018-11-14 18:06:33 +0000

    redirect to :3500 if localhost

--- client-web/src/index.tsx
+++ client-web/src/index.tsx
@@ -11,21 +11,25 @@ import 'tailwindcss/dist/tailwind.min.css';
 import 'text-spinners/spinners.css';
 import './index.css';
 
-Modal.setAppElement('#root');
+if (window.location.href.indexOf('localhost:3051') >= 0) {
+  window.location.href = 'http://localhost:3050/';
+} else {
+  Modal.setAppElement('#root');
 
-const store = init({ models });
-const { dispatch } = store;
-dispatch['ui'].loadMessages().then(() => {
-  document.title = store.getState().ui.messages.title;
+  const store = init({ models });
+  const { dispatch } = store;
+  dispatch['ui'].loadMessages().then(() => {
+    document.title = store.getState().ui.messages.title;
 
-  ReactDOM.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>,
-    document.getElementById('root') as HTMLElement,
-  );
-});
+    ReactDOM.render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>,
+      document.getElementById('root') as HTMLElement,
+    );
+  });
 
-registerServiceWorker();
+  registerServiceWorker();
+}
